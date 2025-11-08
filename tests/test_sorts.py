d74e628f0558f215510721db0496cd115ba13244
Katya Tupikova <k_tupikova00@mail.ru>
2025-11-08 13:58:36 +0300

    Add tests for sorting

---
+++ tests/test_sorts.py
@@ -0,0 +1,42 @@
+from spbu_sorting import b_sort, h_sort, m_sort, q_sort
+import pytest
+
+
+TEST_CASES = [
+    ([], [], "empty list"),
+    ([1], [1], "one elem"),
+    ([1, 1], [1, 1], "two elem"),
+    ([1, 2, 3], [1, 2, 3], "already sorted"),
+    ([3, 2, 1], [1, 2, 3], "reversed"),
+    ([1, 3, 2], [1, 2, 3], "mixed")
+]
+
+SORTING_FUNCTIONS = [
+    (b_sort, "bubble_sort"),
+    (h_sort, "heap sort"),
+    (m_sort, "merge sort"),
+    (q_sort, "quick sort")
+]
+
+@pytest.mark.parametrize("arr,expected,test_name", TEST_CASES)
+@pytest.mark.parametrize("function,func_name", SORTING_FUNTIONS)
+def test_preliminary(arr, expected, test_name, function, func_name):
+    assert function(arr) == expected
+
+
+@pytest.mark.parametrize("function, func_name")
+def test_small_random(function, func_name):
+    arr = small_random_list
+    assert function(arr) == sorted(arr)
+
+
+@pytest.mark.parametrize("function, func_name")
+def test_big_random(function, func_name):
+    arr = big_random_list
+    assert function(arr) == sorted(arr)
+
+
+@pytest.mark.parametrize("function, func_name")
+def test_random_size(function, func_name):
+    arr = random_size_list
+    assert function(arr) == sorted(arr)
