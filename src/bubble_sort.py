08d25fc6b9ef0bafbd3282f65f2ed0277a238748
Katya Tupikova <k_tupikova00@mail.ru>
2025-11-08 10:27:33 +0300

    Add bubble sort

---
+++ src/bubble_sort.py
@@ -0,0 +1,9 @@
+def b_sort(arr):
+    was_swap = 1
+    arr_lenght = len(arr)
+    while was_swap:
+        was_swap = 0
+        for i in range(1, arr_lenght):
+            if arr[i - 1] > arr[i]:
+                arr[i - 1], arr[i] = arr[i], arr[i - 1]
+                was_swap = 1
