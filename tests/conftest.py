639be031f11b4f5686fda0f88780cb8186489aeb
Katya Tupikova <k_tupikova00@mail.ru>
2025-11-08 13:30:54 +0300

    Add fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+from random import randint
+
+
+@pytest.fixture
+def small_random_list():
+    return [randint(-100, 100) for _ in range(100)]
+
+
+@pytest.fixture
+def big_random_list():
+    return [randint(-10000, 10000) for _ in range(10000)]
+
+
+@pytest.fixture
+def random_size_list():
+    return [randint(-1000, 1000) for _ in range(randint(1, 1000))]
+
