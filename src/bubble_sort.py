2b40060b3afcaa3ee80d80d606d846c6e5b2ea5e
Katya Tupikova <k_tupikova00@mail.ru>
2025-11-08 12:12:26 +0300

    add function (was procedure)

--- src/bubble_sort.py
+++ src/bubble_sort.py
@@ -1,4 +1,4 @@
-def b_sort(arr):
+def __b_sort(arr):
     was_swap = 1
     arr_lenght = len(arr)
     while was_swap:
@@ -7,3 +7,7 @@ def b_sort(arr):
             if arr[i - 1] > arr[i]:
                 arr[i - 1], arr[i] = arr[i], arr[i - 1]
                 was_swap = 1
+
+def b_sort(arr):
+    __b_sort(arr[:])
+    return arr
